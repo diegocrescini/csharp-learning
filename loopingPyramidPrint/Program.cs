34bb281ecfb116ba2de1b05bb3df7800bd7ba501
Diego Crescini <diegocrescini@gmail.com>
2026-08-08 13:24:30 -0300

    Finishes The C# Player's Guide ch12 challenge: print a pyramid with for loop usinn * only once.

---
+++ loopingPyramidPrint/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int spaces = 4;
+int asterisks = 1;
+for (int row = 0; row < 5; row++) // for rows 0-4
+{
+
+    for (int i  = spaces; i > 0; i--) // for 
+    {
+        Console.Write(" ");
+        
+    }
+    for (int j = 0; j < asterisks; j ++)
+    {
+        Console.Write("*");
+        
+    }
+    
+    spaces--;
+    asterisks+=2;
+    Console.WriteLine();
+}
+
+
